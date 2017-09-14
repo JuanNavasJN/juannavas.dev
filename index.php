7303fe702710cc11d61292d1ddc6f0688f807048
Juan Navas <navascruzf@gmail.com>
2017-09-14 10:12:11 -0400

    Configuracion incial

---
+++ index.php
@@ -0,0 +1,53 @@
+<?php 
+
+	$errores = '';
+	$enviado = '';
+
+	if (isset($_POST['submit'])) {
+		$nombre = $_POST['nombre'];
+		$correo = $_POST['correo'];
+		$mensaje = $_POST['mensaje'];
+
+		if (!empty($nombre)) {
+			$nombre = trim($nombre);
+			$nombre = filter_var($nombre, FILTER_SANITIZE_STRING);
+		} else {
+			$errores .= 'Por favor ingresa un nombre <br>';
+		}
+
+
+		if (!empty($correo)) {
+			$correo = filter_var($correo, FILTER_SANITIZE_EMAIL);
+
+			if (!filter_var($correo, FILTER_VALIDATE_EMAIL)) {
+				$errores .= 'Por favor ingresa un correo valido <br>';
+			}
+
+		} else {
+			$errores .= 'Por favor ingresa un correo <br>';
+		}
+
+		if (!empty($mensaje)) {
+			$mensaje = htmlspecialchars($mensaje);
+			$mensaje = trim($mensaje);
+			$mensaje = stripcslashes($mensaje);
+		} else {
+			$errores .= 'Por favor ingresa el mensaje <br>';
+		}
+
+		if(!$errores){
+			$enviarA = 'navascruzf@gmail.com';
+			$asunto = 'Correo enviado desde juannavas.000webhostapp.com';
+			$mensajePreparado = "De: $nombre \n";
+			$mensajePreparado .= "Correo: $correo \n";
+			$mensajePreparado .= "Mensaje: " . $mensaje; 
+
+			mail($enviarA, $asunto, $mensajePreparado);
+			$enviado = true;
+		}
+
+	}
+
+	require "index.view.php";
+
+ ?>
