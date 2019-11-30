e7e81a061527898b2e73a890170d87794883a941
Juan <navascruzf@gmail.com>
2019-11-30 09:17:02 -0400

    initial commit v3

---
+++ src/resumeData.js
@@ -0,0 +1,108 @@
+let resumeData = {
+    imagebaseurl: "https://juannavas.xyz",
+    name: "Juan Navas",
+    role: "",
+    linkedinId: "Your LinkedIn Id",
+    skypeid: "Your skypeid",
+    roleDescription:
+        "I'm Web Developer Full Stack, I works with react.js, node.js, laravel, mysql, mongodb, docker, git, ... I never stop learning. Also, I'm Science Computer student at the Universidad Central de Venezuela. ",
+    socialLinks: [
+        {
+            name: "linkedin",
+            url: "https://www.linkedin.com/in/juannavasjn",
+            className: "fa fa-linkedin"
+        },
+        {
+            name: "github",
+            url: "http://github.com/juannavasjn",
+            className: "fa fa-github"
+        },
+        {
+            name: "gitlab",
+            url: "http://gitlab.com/juannavasjn",
+            className: "fab fa-gitlab"
+        }
+    ],
+    aboutme:
+        "I am currently a pre-final year student at The LNM Institute of Information Technology and pursuing my B.Tech from here. I am a self taught Full Stack Web Developer, currently diving deeper into Machine Learning. I believe that to be successful in life, one needs to be obsessive with their dreams and keep working towards them.",
+    address: "India",
+    website: "https://rbhatia46.github.io",
+    education: [
+        {
+            UniversityName: "The LNM Insitute of Information Technology",
+            specialization: "Some specialization",
+            MonthOfPassing: "Aug",
+            YearOfPassing: "2020",
+            Achievements: "Some Achievements"
+        },
+        {
+            UniversityName: "Some University",
+            specialization: "Some specialization",
+            MonthOfPassing: "Jan",
+            YearOfPassing: "2018",
+            Achievements: "Some Achievements"
+        }
+    ],
+    work: [
+        {
+            CompanyName: "Some Company",
+            specialization: "Some specialization",
+            MonthOfLeaving: "Jan",
+            YearOfLeaving: "2018",
+            Achievements: "Some Achievements"
+        },
+        {
+            CompanyName: "Some Company",
+            specialization: "Some specialization",
+            MonthOfLeaving: "Jan",
+            YearOfLeaving: "2018",
+            Achievements: "Some Achievements"
+        }
+    ],
+    skillsDescription: "Your skills here",
+    skills: [
+        {
+            skillname: "HTML5"
+        },
+        {
+            skillname: "CSS"
+        },
+        {
+            skillname: "Reactjs"
+        }
+    ],
+    portfolio: [
+        {
+            name: "project1",
+            description: "mobileapp",
+            imgurl: "images/portfolio/phone.jpg"
+        },
+        {
+            name: "project2",
+            description: "mobileapp",
+            imgurl: "images/portfolio/project.jpg"
+        },
+        {
+            name: "project3",
+            description: "mobileapp",
+            imgurl: "images/portfolio/project2.png"
+        },
+        {
+            name: "project4",
+            description: "mobileapp",
+            imgurl: "images/portfolio/phone.jpg"
+        }
+    ],
+    testimonials: [
+        {
+            description: "This is a sample testimonial",
+            name: "Some technical guy"
+        },
+        {
+            description: "This is a sample testimonial",
+            name: "Some technical guy"
+        }
+    ]
+};
+
+export default resumeData;
