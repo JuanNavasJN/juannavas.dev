bad2796afdaa498750fa298a29ef08d49cf69931
Juan Navas <navascruzf@gmail.com>
2020-10-04 13:14:37 -0400

    removed matomo

--- pages/_app.js
+++ pages/_app.js
@@ -24,28 +24,6 @@ export default function MyApp({ Component, pageProps }) {
                     rel="stylesheet"
                     href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"
                 />
-
-                {/* <!-- Matomo --> */}
-
-                <script
-                    type="text/javascript"
-                    dangerouslySetInnerHTML={{
-                        __html: `
-             var _paq = window._paq = window._paq || [];
-             /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
-             _paq.push(['trackPageView']);
-             _paq.push(['enableLinkTracking']);
-             (function() {
-                 var u="//matomo.juannavas.dev/";
-                 _paq.push(['setTrackerUrl', u+'matomo.php']);
-                 _paq.push(['setSiteId', '1']);
-                 var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
-                 g.type='text/javascript'; g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
-             })();            
-            `,
-                    }}
-                />
-                {/* <!-- End Matomo Code --> */}
             </Head>
             <Component {...pageProps} />
         </Fragment>
