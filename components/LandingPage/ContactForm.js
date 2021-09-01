3b931d75b8a81aa91a8b245f93fe0d54760b2a77
Juan Navas <iam@juannavas.dev>
2021-09-01 16:18:24 -0400

    remove contact form

--- components/LandingPage/ContactForm.js
+++ components/LandingPage/ContactForm.js
@@ -59,103 +59,11 @@ const ContactForm = () => {
             <div className="container">
                 <div className="section-title three">
                     <span className="sub-title">CONTACT</span>
-                    <h2>
-                        Yes! You Are Here! Have You Any Project? Drop A Line
-                        Here
-                    </h2>
+                    <h2>Have You Any Project?</h2>
+                    <h2>Feel Free To Email Me</h2>
                 </div>
 
                 <div className="row align-items-center">
-                    <div className="col-md-7 col-lg-6">
-                        <form
-                            id="contactForm"
-                            onSubmit={handleSubmit(onSubmit)}
-                        >
-                            <div className="form-group">
-                                <input
-                                    type="text"
-                                    name="name"
-                                    className="form-control"
-                                    placeholder="Name"
-                                    value={contact.name}
-                                    onChange={handleChange}
-                                    ref={register({ required: true })}
-                                />
-                                <div
-                                    className="invalid-feedback"
-                                    style={{ display: "block" }}
-                                >
-                                    {errors.name && "Name is required."}
-                                </div>
-                            </div>
-
-                            <div className="form-group">
-                                <input
-                                    type="text"
-                                    name="email"
-                                    className="form-control"
-                                    placeholder="Email"
-                                    value={contact.email}
-                                    onChange={handleChange}
-                                    ref={register({
-                                        required: true,
-                                        pattern: /^\S+@\S+$/i,
-                                    })}
-                                />
-                                <div
-                                    className="invalid-feedback"
-                                    style={{ display: "block" }}
-                                >
-                                    {errors.email && "Email is required."}
-                                </div>
-                            </div>
-
-                            <div className="form-group">
-                                <input
-                                    type="text"
-                                    name="subject"
-                                    className="form-control"
-                                    placeholder="Subject"
-                                    value={contact.subject}
-                                    onChange={handleChange}
-                                    ref={register({ required: true })}
-                                />
-                                <div
-                                    className="invalid-feedback"
-                                    style={{ display: "block" }}
-                                >
-                                    {errors.subject && "Subject is required."}
-                                </div>
-                            </div>
-
-                            <div className="form-group">
-                                <textarea
-                                    name="text"
-                                    className="form-control"
-                                    cols="30"
-                                    rows="6"
-                                    placeholder="Write message"
-                                    value={contact.text}
-                                    onChange={handleChange}
-                                    ref={register({ required: true })}
-                                />
-                                <div
-                                    className="invalid-feedback"
-                                    style={{ display: "block" }}
-                                >
-                                    {errors.text && "Text body is required."}
-                                </div>
-                            </div>
-
-                            <button
-                                type="submit"
-                                className="btn common-btn three"
-                            >
-                                Send Message <span></span>
-                            </button>
-                        </form>
-                    </div>
-
                     <div className="col-md-5 col-lg-6">
                         <div className="contact-content">
                             <div className="top">
