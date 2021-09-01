d91630b1807e0e480056c017ee68175bef20da20
Juan Navas <iam@juannavas.dev>
2021-09-01 12:16:55 -0400

    Remove preloader

--- components/_App/Layout.js
+++ components/_App/Layout.js
@@ -1,16 +1,8 @@
 import React from "react";
 import Head from "next/head";
 import GoTop from "./GoTop";
-import Preloader from "./Preloader";
 
 const Layout = ({ children }) => {
-    // Preloader
-    const [loader, setLoader] = React.useState(true);
-
-    React.useEffect(() => {
-        setTimeout(() => setLoader(false), 1500);
-    }, []);
-
     return (
         <React.Fragment>
             <Head>
@@ -36,8 +28,6 @@ const Layout = ({ children }) => {
             </Head>
 
             {children}
-
-            {loader ? <Preloader /> : null}
 
             <GoTop scrollStepInPx="100" delayInMs="10.50" />
         </React.Fragment>
