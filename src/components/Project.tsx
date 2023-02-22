7ff4e98efe2d9a230a3726ee22749978d3dac228
Juan Navas <iam@juannavas.dev>
2023-02-22 17:19:12 -0400

    Add image buttons

--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -1,6 +1,7 @@
 import { FC } from 'react';
 import React from 'react';
 import { Nunito } from '@next/font/google';
+import LinkButton from './LinkButton';
 
 import styles from '@/styles/Project.module.css';
 
@@ -21,13 +22,19 @@ const Project: FC<ProjectProps> = ({
 }) => {
   return (
     <div className={styles.project}>
-      <div></div>
+      <div>
+        <div className={styles.image}></div>
+      </div>
       <div>
         <h4 className={nunito.className}>{title}</h4>
         <p className={nunito.className}>{description}</p>
         <p className={nunito.className}>
           <b>Tech Stack:</b> {techStack}
         </p>
+        <div className={styles.linksContainer}>
+          <LinkButton text="Site" href="" />
+          <LinkButton text="Code" href="" />
+        </div>
       </div>
     </div>
   );
