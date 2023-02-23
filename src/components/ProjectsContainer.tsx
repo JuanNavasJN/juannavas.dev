1ea56ff6ceeca524cc2546c967dbc11f2c0cb35d
Juan Navas <iam@juannavas.dev>
2023-02-23 11:30:49 -0400

    Add initial projects data

--- src/components/ProjectsContainer.tsx
+++ src/components/ProjectsContainer.tsx
@@ -1,34 +1,8 @@
 import { FC } from 'react';
 import ProjectCard from './ProjectCard';
+import { projects } from '@/data/projects';
 
 import styles from '@/styles/ProjectsContainer.module.css';
-
-const projects = [
-  {
-    id: 1,
-    title: 'Project Title',
-    description:
-      'Quis magna adipisicing occaecat minim eiusmod adipisicing sit nisi consectetur. Laborum sunt ad irure tempor ea irure in Lorem voluptate nulla non culpa cillum mollit. Commodo eiusmod laboris anim sint enim magna reprehenderit labore nostrud officia. ',
-    techStack: 'React, Next.js, Redux, Node.js, MongoDB, AWS.',
-    imgPath: ''
-  },
-  {
-    id: 2,
-    title: 'Project Title',
-    description:
-      'Quis magna adipisicing occaecat minim eiusmod adipisicing sit nisi consectetur. Laborum sunt ad irure tempor ea irure in Lorem voluptate nulla non culpa cillum mollit. Commodo eiusmod laboris anim sint enim magna reprehenderit labore nostrud officia. ',
-    techStack: 'React, Next.js, Redux, Node.js, MongoDB, AWS.',
-    imgPath: ''
-  },
-  {
-    id: 3,
-    title: 'Project Title',
-    description:
-      'Quis magna adipisicing occaecat minim eiusmod adipisicing sit nisi consectetur. Laborum sunt ad irure tempor ea irure in Lorem voluptate nulla non culpa cillum mollit. Commodo eiusmod laboris anim sint enim magna reprehenderit labore nostrud officia. ',
-    techStack: 'React, Next.js, Redux, Node.js, MongoDB, AWS.',
-    imgPath: ''
-  }
-];
 
 const ProjectsContainer: FC = () => {
   return (
---
+++ src/data/projects.ts
@@ -0,0 +1,86 @@
+export const projects = [
+  {
+    id: 9,
+    title: 'SmartAnnie',
+    description:
+      'Quis magna adipisicing occaecat minim eiusmod adipisicing sit nisi consectetur. Laborum sunt ad irure tempor ea irure in Lorem voluptate nulla non culpa cillum mollit. Commodo eiusmod laboris anim sint enim magna reprehenderit labore nostrud officia.',
+    techStack: 'React, Next.js, Redux, Node.js, MongoDB, AWS.',
+    imgPath: '',
+    show: true
+  },
+  {
+    id: 8,
+    title: 'APM Music',
+    description:
+      'Quis magna adipisicing occaecat minim eiusmod adipisicing sit nisi consectetur. Laborum sunt ad irure tempor ea irure in Lorem voluptate nulla non culpa cillum mollit. Commodo eiusmod laboris anim sint enim magna reprehenderit labore nostrud officia.',
+    techStack: 'React, Next.js, Redux, Node.js, MongoDB, AWS.',
+    imgPath: '',
+    show: true
+  },
+  {
+    id: 7,
+    title: 'GlufComercios',
+    description:
+      'Quis magna adipisicing occaecat minim eiusmod adipisicing sit nisi consectetur. Laborum sunt ad irure tempor ea irure in Lorem voluptate nulla non culpa cillum mollit. Commodo eiusmod laboris anim sint enim magna reprehenderit labore nostrud officia.',
+    techStack: 'React, Next.js, Redux, Node.js, MongoDB, AWS.',
+    imgPath: '',
+    show: true
+  },
+  {
+    id: 6,
+    title: 'Glufco',
+    description:
+      'Quis magna adipisicing occaecat minim eiusmod adipisicing sit nisi consectetur. Laborum sunt ad irure tempor ea irure in Lorem voluptate nulla non culpa cillum mollit. Commodo eiusmod laboris anim sint enim magna reprehenderit labore nostrud officia.',
+    techStack: 'React, Next.js, Redux, Node.js, MongoDB, AWS.',
+    imgPath: '',
+    show: true
+  },
+  {
+    id: 5,
+    title: 'Logicol',
+    description:
+      'Quis magna adipisicing occaecat minim eiusmod adipisicing sit nisi consectetur. Laborum sunt ad irure tempor ea irure in Lorem voluptate nulla non culpa cillum mollit. Commodo eiusmod laboris anim sint enim magna reprehenderit labore nostrud officia.',
+    techStack: 'React, Next.js, Redux, Node.js, MongoDB, AWS.',
+    imgPath: '',
+    site: 'https://logicol.es/',
+    show: true
+  },
+  {
+    id: 4,
+    title: 'AdminTools',
+    description:
+      'Quis magna adipisicing occaecat minim eiusmod adipisicing sit nisi consectetur. Laborum sunt ad irure tempor ea irure in Lorem voluptate nulla non culpa cillum mollit. Commodo eiusmod laboris anim sint enim magna reprehenderit labore nostrud officia.',
+    techStack: 'React, Next.js, Redux, Node.js, MongoDB, AWS.',
+    imgPath: '',
+    show: true
+  },
+  {
+    id: 3,
+    title: 'Orion - Trading Terminal',
+    description:
+      'Quis magna adipisicing occaecat minim eiusmod adipisicing sit nisi consectetur. Laborum sunt ad irure tempor ea irure in Lorem voluptate nulla non culpa cillum mollit. Commodo eiusmod laboris anim sint enim magna reprehenderit labore nostrud officia.',
+    techStack: 'React, Next.js, Redux, Node.js, MongoDB, AWS.',
+    imgPath: '',
+    code: 'https://github.com/orionprotocol/trading-terminal',
+    show: true
+  },
+  {
+    id: 2,
+    title: 'Akowe',
+    description:
+      'Quis magna adipisicing occaecat minim eiusmod adipisicing sit nisi consectetur. Laborum sunt ad irure tempor ea irure in Lorem voluptate nulla non culpa cillum mollit. Commodo eiusmod laboris anim sint enim magna reprehenderit labore nostrud officia.',
+    techStack: 'React, Next.js, Redux, Node.js, MongoDB, AWS.',
+    imgPath: '',
+    site: 'https://akowe-landing.vercel.app/',
+    show: true
+  },
+  {
+    id: 1,
+    title: 'CryptoTaxMate',
+    description:
+      'Quis magna adipisicing occaecat minim eiusmod adipisicing sit nisi consectetur. Laborum sunt ad irure tempor ea irure in Lorem voluptate nulla non culpa cillum mollit. Commodo eiusmod laboris anim sint enim magna reprehenderit labore nostrud officia.',
+    techStack: 'React, Next.js, Redux, Node.js, MongoDB, AWS.',
+    imgPath: '',
+    show: true
+  }
+];
