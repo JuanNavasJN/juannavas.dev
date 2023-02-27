2326b51939d35ac29cdd161fff8dce1ed5a3c8c6
Juan Navas <iam@juannavas.dev>
2023-02-27 11:15:01 -0400

    Write down project descriptions

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -1,55 +1,18 @@
 export const projects = [
   {
-    id: 9,
-    title: 'SmartAnnie',
-    description:
-      'Quis magna adipisicing occaecat minim eiusmod adipisicing sit nisi consectetur. Laborum sunt ad irure tempor ea irure in Lorem voluptate nulla non culpa cillum mollit. Commodo eiusmod laboris anim sint enim magna reprehenderit labore nostrud officia.',
-    techStack: 'React, Styled Components, Express.js, MongoDB, Heroku.',
-    imgPath: '/images/smartannie.png',
-    show: true
-  },
-  {
     id: 8,
     title: 'APM Music',
     description:
-      'Quis magna adipisicing occaecat minim eiusmod adipisicing sit nisi consectetur. Laborum sunt ad irure tempor ea irure in Lorem voluptate nulla non culpa cillum mollit. Commodo eiusmod laboris anim sint enim magna reprehenderit labore nostrud officia.',
+      'APM Music is a creative music house and production music library. They offer a wide variety of choices for digital access to their music, including their state-of-the-art search, download and project management application. I implemented some UI features of their new redesigned web app.',
     techStack: 'React, Next.js, Redux, SASS.',
     imgPath: '/images/apm.png',
-    show: true
-  },
-  {
-    id: 7,
-    title: 'GlufComercios',
-    description:
-      'Quis magna adipisicing occaecat minim eiusmod adipisicing sit nisi consectetur. Laborum sunt ad irure tempor ea irure in Lorem voluptate nulla non culpa cillum mollit. Commodo eiusmod laboris anim sint enim magna reprehenderit labore nostrud officia.',
-    techStack: 'Vue.js, Nuxt.js, GraphQL, Mapbox.',
-    imgPath: '/images/glufcomercios.png',
-    show: true
-  },
-  {
-    id: 6,
-    title: 'Glufco',
-    description:
-      'Quis magna adipisicing occaecat minim eiusmod adipisicing sit nisi consectetur. Laborum sunt ad irure tempor ea irure in Lorem voluptate nulla non culpa cillum mollit. Commodo eiusmod laboris anim sint enim magna reprehenderit labore nostrud officia.',
-    techStack: 'Vue.js, Nuxt.js, Laravel, PostgreSQL.',
-    imgPath: '',
-    show: false
-  },
-  {
-    id: 5,
-    title: 'Logicol',
-    description:
-      'Quis magna adipisicing occaecat minim eiusmod adipisicing sit nisi consectetur. Laborum sunt ad irure tempor ea irure in Lorem voluptate nulla non culpa cillum mollit. Commodo eiusmod laboris anim sint enim magna reprehenderit labore nostrud officia.',
-    techStack: 'React, GraphQL.',
-    imgPath: '/images/logicol.png',
-    // site: 'https://logicol.es/',
     show: true
   },
   {
     id: 4,
     title: 'AdminTools',
     description:
-      'Quis magna adipisicing occaecat minim eiusmod adipisicing sit nisi consectetur. Laborum sunt ad irure tempor ea irure in Lorem voluptate nulla non culpa cillum mollit. Commodo eiusmod laboris anim sint enim magna reprehenderit labore nostrud officia.',
+      'A web app to provide server monitoring services. I worked on the implementation of some features like third-party authentication, handling multiple role and permission, a support ticket system and a one-to-one real-time chat.',
     techStack:
       'React, Redux, Express.js, TypeScript, Socket.IO, MongoDB, Digital Ocean.',
     imgPath: '/images/admintools.png',
@@ -59,29 +22,63 @@ export const projects = [
     id: 3,
     title: 'Orion - Trading Terminal',
     description:
-      'Quis magna adipisicing occaecat minim eiusmod adipisicing sit nisi consectetur. Laborum sunt ad irure tempor ea irure in Lorem voluptate nulla non culpa cillum mollit. Commodo eiusmod laboris anim sint enim magna reprehenderit labore nostrud officia.',
+      'A web app that always searches for the best prices on the market, allowing you to carry out arbitrage opportunities without the need for accounts on multiple exchanges. I contributed to the initial rebuilt of the React app for a better performance and integrability.',
     techStack: 'React, Redux, Web3, WebSocket.',
     imgPath: '/images/orion.png',
-    code: 'https://github.com/orionprotocol/trading-terminal',
+    // code: 'https://github.com/orionprotocol/trading-terminal',
     show: true
-  },
-  {
-    id: 2,
-    title: 'Akowe',
-    description:
-      'Quis magna adipisicing occaecat minim eiusmod adipisicing sit nisi consectetur. Laborum sunt ad irure tempor ea irure in Lorem voluptate nulla non culpa cillum mollit. Commodo eiusmod laboris anim sint enim magna reprehenderit labore nostrud officia.',
-    techStack: 'React.',
-    imgPath: '/images/akowe.png',
-    site: 'https://akowe-landing.vercel.app/',
-    show: false
   },
   {
     id: 1,
     title: 'CryptoTaxMate',
     description:
-      'Quis magna adipisicing occaecat minim eiusmod adipisicing sit nisi consectetur. Laborum sunt ad irure tempor ea irure in Lorem voluptate nulla non culpa cillum mollit. Commodo eiusmod laboris anim sint enim magna reprehenderit labore nostrud officia.',
+      'A web app to track cryptocurrency exchange transactions by API keys or CSV file. It allows you to see all the data in one dashboard through tables, charts and downloadable reports. I implemented the whole first version of the solution from scratch.',
     techStack: 'React, Redux, Laravel, MySQL, GCP.',
     imgPath: '/images/ctm.png',
     show: true
+  },
+  {
+    id: 9,
+    title: 'SmartAnnie',
+    description:
+      'A simple scheduling web app to make easier the booking appointment process between solo-entrepreneur and customer. I implemented UI features and integrated Stripe for payment subscription.',
+    techStack: 'React, Styled Components, Express.js, MongoDB, Heroku.',
+    imgPath: '/images/smartannie.png',
+    show: true
+  },
+  {
+    id: 7,
+    title: 'GlufComercios',
+    description:
+      'A web app to list shops with their contact, location and payment method information, including a map to show their specific locations. I built the whole first version from scratch.',
+    techStack: 'Vue.js, Nuxt.js, GraphQL, Mapbox.',
+    imgPath: '/images/glufcomercios.png',
+    show: true
+  },
+  {
+    id: 6,
+    title: 'Glufco',
+    description: '',
+    techStack: 'Vue.js, Nuxt.js, Laravel, PostgreSQL.',
+    imgPath: '',
+    show: false
+  },
+  {
+    id: 5,
+    title: 'Logicol',
+    description: '',
+    techStack: 'React, GraphQL.',
+    imgPath: '/images/logicol.png',
+    // site: 'https://logicol.es/',
+    show: false
+  },
+  {
+    id: 2,
+    title: 'Akowe',
+    description: '',
+    techStack: 'React.',
+    imgPath: '/images/akowe.png',
+    site: 'https://akowe-landing.vercel.app/',
+    show: false
   }
 ];
