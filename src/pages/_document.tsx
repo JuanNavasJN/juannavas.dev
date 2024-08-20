5337e94919eac9ba0dc3530019698bca6bb07b8a
Juan Navas <iam@juannavas.dev>
2024-08-20 17:37:00 -0400

    update analytics source

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -24,7 +24,7 @@ export default function Document() {
           async
           defer
           data-website-id="54890e73-54f0-48b9-9ff1-9d8b77c2de99"
-          src="https://analytics.juannavas.dev/umami.js"
+          src="https://analytics.juannavas.dev/script.js"
         ></script>
       </Head>
       <body>
