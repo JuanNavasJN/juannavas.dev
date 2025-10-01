07d206d368a1431ca5f2a905f0960bd3302f5ba6
Juan Navas <iam@juannavas.dev>
2025-10-01 14:23:27 -0400

    Add ChipsPlay

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -1,12 +1,12 @@
 export const projects = [
   {
     id: 10,
-    title: 'Betcoin Token',
+    title: 'ChipsPlay',
     description:
       'A web app to manage online casino chips (loads and withdrawals). Some features I developed: tables to handle users and chip requests, real-time chat, connection to external APIs, implemented queues to handle high workload, Excel reports...',
     techStack:
       'Next.js, Tailwind CSS, NestJS, MySQL, Redis, Docker, AWS, Socket.io, bull (queues).',
-    imgPath: '/images/betcoin-token.png',
+    imgPath: '/images/chips-play.png',
     show: true
   },
   {
